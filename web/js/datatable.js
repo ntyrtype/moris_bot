c5d2734dc02731f9993dfa170757f67d17e8042b
erianaerwt <erianaerawati22@gmail.com>
2025-02-13 09:24:49 +0700

    css

--- web/js/datatable.js
+++ web/js/datatable.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     $('#dataTable').DataTable({
+        "scrollX": true,
+        "scrollY": true,
         "ordering": false // Menonaktifkan fitur sortir
     });
 });
