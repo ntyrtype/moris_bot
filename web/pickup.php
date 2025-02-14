7bdac5e1c15596875f500ade8dd220fe357967a8
ntyrtype <123.falah.fajrulahmad@gmail.com>
2025-02-14 10:02:20 +0700

    title

--- web/pickup.php
+++ web/pickup.php
@@ -143,7 +143,7 @@ $orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style/style.css">
-    <title>Order</title>
+    <title>Pickup</title>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
