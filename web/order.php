0daad4c9c511ceff84588a44c4b4b0cb7cce0c2c
ntyrtype <123.falah.fajrulahmad@gmail.com>
2025-02-14 11:09:33 +0700

    fix download order pickup

--- web/order.php
+++ web/order.php
@@ -179,6 +179,7 @@ $orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.0/xlsx.full.min.js"></script>
 </head>
 <body>
 
